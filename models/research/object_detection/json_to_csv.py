ee14302049e3990d73ebefcbc49084359d5e9b4a
saeedarabi92 <arabi@iastate.edu>
2019-05-16 14:42:30 -0500

    feat: Add json_to_csv.py

---
+++ models/research/object_detection/json_to_csv.py
@@ -0,0 +1,39 @@
+import os
+import glob
+import pandas as pd
+import csv
+import json
+from ast import literal_eval
+
+
+
+def json_to_csv(path):
+    json_list = []
+    for json_file in glob.glob(path + '/*.json'):
+        print('json file: ', json_file)
+        json_object = json.load(open(json_file))
+        for i in range(len(json_object["shapes"])):
+            value = (json_object['imagePath'],
+                    json_object['imageHeight'],
+                    json_object['imageWidth'],
+                    json_object['shapes'][i]['label'],
+                    json_object['shapes'][i]['points'][0][0],
+                    json_object['shapes'][i]['points'][0][1],
+                    json_object['shapes'][i]['points'][1][0],
+                    json_object['shapes'][i]['points'][1][1])
+            json_list.append(value)
+    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    json_df = pd.DataFrame(json_list, columns=column_name)
+    return json_df
+
+def main():
+    for folder in ['train','test']:
+        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+        print('image_path: ', image_path)
+        json_df = json_to_csv(image_path)
+        json_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        print('Successfully converted json to csv.')
+
+
+if __name__ == "__main__":
+    main()
